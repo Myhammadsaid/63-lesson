0596f12fd94394d3785aa78c59a77c8cf88fc746
Mуhammadsaid <muhammadsaidmm4@gmail.com>
2024-04-07 10:06:32 +0300

    appdat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import musor from "./musor.png";
+import { toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const [name, setName] = useState("");
+  const [data, setData] = useState(
+    JSON.parse(localStorage.getItem("data")) || []
+  );
+
+  const handle = (e) => {
+    e.preventDefault();
+    if (!name.trim()) return toast.error("Iltimos maydoni to'ldiring");
+    let user = {
+      name,
+    };
+    setData([...data, user]);
+    setName("");
+  };
+
+  const handleDelete = (index) => {
+    const newData = [...data];
+    newData.splice(index, 1);
+    setData(newData);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(data));
+  }, [data]);
+
+  let users = data?.map((u, ix) => (
+    <div className="user-div" key={ix}>
+      <p className="user" title={u.name}>
+        {u.name}
+      </p>
+      <button className="user-btn" onClick={() => handleDelete(ix)}>
+        <img className="img" style={{ width: "25px" }} src={musor} />
+      </button>
+    </div>
+  ));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer />
+      <div>
+        <form action="" onSubmit={handle}>
+          <input
+            required
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            placeholder="Name"
+          />
+          <button>Add</button>
+        </form>
+      </div>
+      <div>{users}</div>
     </div>
   );
 }
